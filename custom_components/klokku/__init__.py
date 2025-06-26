1a1ad19d5aecc81ca8879be41be687be34b23c99
Mariusz Józala <mariusz@aetas.pl>
2025-06-27 00:26:43 +0200

    Remove redundant TODO comment

--- custom_components/klokku/__init__.py
+++ custom_components/klokku/__init__.py
@@ -30,6 +30,3 @@ async def async_setup_entry(hass: HomeAssistant, entry: KlokkuConfigEntry) -> bo
 async def async_unload_entry(hass: HomeAssistant, entry: KlokkuConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
-
-
-# TODO - add something to README.md
